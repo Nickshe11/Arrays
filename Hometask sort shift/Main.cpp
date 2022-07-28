c384af44ffec07e4bd0516f30d7d800e5c081146
Nickshe11 <shelobod@gmail.com>
2022-07-28 16:29:48 +0300

    Hometask 2d everything but comparison

--- Hometask sort shift/Main.cpp
+++ Hometask sort shift/Main.cpp
@@ -1,5 +1,9 @@
 #include<iostream>
 #define tab "\t"
+#define delimiter "--------------------------------"
+//#define SORT
+//#define UniqueRandom
+#define Comparison
 using namespace std;
 
 void main()
@@ -8,7 +12,8 @@ void main()
 	const int ROWS = 3;//строки
 	const int COLS = 4;//столбцы
 	int arr[ROWS][COLS]{};
-	int box;
+	int box, mark = 0;
+#ifdef SORT
 	for (int i = 0; i < ROWS; i++)
 	{
 		for (int j = 0; j < COLS; j++)
@@ -31,7 +36,7 @@ void main()
 		{
 			for (int k = i; k < ROWS; k++)
 			{
-				switch (k>i)
+				switch (k > i)
 				{
 				case(0):
 					for (int l = j; l < COLS; l++)
@@ -56,9 +61,6 @@ void main()
 					}
 					break;
 				}
-
-
-
 			}
 		}
 	}
@@ -72,4 +74,128 @@ void main()
 		}
 		cout << endl;
 	}
+#endif // SORT
+
+#ifdef UniqueRandom
+	// Заполнение массива уникальными числами
+
+	cout << delimiter << "\nЗаполнение массива уникальными числами" << endl;
+	mark = 0;
+	int brr[ROWS][COLS]{};
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			brr[i][j] = rand() % 100;
+			if (j == 0)
+			{
+				for (int k = i - 1; k >= 0; k--)
+				{
+					for (int l = COLS - 1; l >= 0; l--)
+					{
+						if (brr[i][j] == brr[k][l])
+						{
+							mark = 1;
+						}
+					}
+				}
+			}
+			else
+			{
+				for (int k = i; k >= 0; k--)
+				{
+					if (k == i)
+					{
+						for (int l = j - 1; l >= 0; l--)
+						{
+							if (brr[i][j] == brr[k][l])
+							{
+								mark = 1;
+							}
+						}
+
+					}
+					else
+					{
+						for (int l = COLS - 1; l >= 0; l--)
+							if (brr[i][j] == brr[k][l])
+							{
+								mark = 1;
+							}
+					}
+				}
+			}
+			if (mark == 1)
+			{
+				j--;
+			}
+
+		}
+	}
+	//Вывод массива после заполнения
+	cout << endl;
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			cout << brr[i][j] << tab;
+		}
+		cout << endl;
+	}
+
+#endif//UniqueRandom
+
+#ifdef Comparison
+
+
+	// Сравнение элементов массива
+
+	cout << delimiter << "\nСравнение элементов массива" << endl;
+	mark = 0;
+	box = 0;
+	int counter = 0;
+	int crr[COLS][ROWS]{};
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			crr[i][j] = rand() % 10;
+		}
+	}
+
+	//Вывод массива после заполнения
+	cout << endl;
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			cout << crr[i][j] << tab;
+		}
+		cout << endl;
+	}
+
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			for (int k = i; k < ROWS; k++)
+			{
+				for (int l = j-1; l < COLS; l++)
+				{
+					if ((crr[i][j]) == crr[k][l])
+					{
+						counter++;//дальше обратное сравнение
+					}
+				}
+			}
+		}
+	}
+
+
+#endif // Comparison
+
+
 }
+
+
+
