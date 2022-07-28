745e2647c3414cbfefbbac00a972a2f4d0a1386b
Nickshe11 <shelobod@gmail.com>
2022-07-28 21:53:07 +0300

    Hometask 2d SORT UNIQUE COMPARISON DONE

--- Hometask sort shift/Main.cpp
+++ Hometask sort shift/Main.cpp
@@ -1,34 +1,27 @@
 #include<iostream>
 #define tab "\t"
 #define delimiter "--------------------------------"
+
 //#define SORT
 //#define UniqueRandom
-#define Comparison
+//#define Comparison
+
 using namespace std;
+const int ROWS = 3;//строки
+const int COLS = 4;//столбцы
+
+void create(int arr[ROWS][COLS], const int ROWS, const int COLS);
+void print(int arr[ROWS][COLS], const int ROWS, const int COLS);
 
 void main()
 {
 	setlocale(LC_ALL, "");
-	const int ROWS = 3;//строки
-	const int COLS = 4;//столбцы
-	int arr[ROWS][COLS]{};
 	int box, mark = 0;
 #ifdef SORT
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			arr[i][j] = rand() % 10;
-		}
-	}
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			cout << arr[i][j] << tab;
-		}
-		cout << endl;
-	}
+	int arr[ROWS][COLS]{};
+	create(arr, ROWS,COLS);
+	print(arr, ROWS, COLS);
+	
 	//Сортировка по возрастанию
 	for (int i = 0; i < ROWS; i++)
 	{
@@ -41,7 +34,7 @@ void main()
 				case(0):
 					for (int l = j; l < COLS; l++)
 					{
-						if (arr[i][j] < arr[k][l])
+						if (arr[i][j] > arr[k][l])
 						{
 							box = arr[i][j];
 							arr[i][j] = arr[k][l];
@@ -52,7 +45,7 @@ void main()
 				case (1):
 					for (int l = 0; l < COLS; l++)
 					{
-						if (arr[i][j] < arr[k][l])
+						if (arr[i][j] > arr[k][l])
 						{
 							box = arr[i][j];
 							arr[i][j] = arr[k][l];
@@ -66,14 +59,7 @@ void main()
 	}
 	//Вывод массива после сортировки
 	cout << endl;
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			cout << arr[i][j] << tab;
-		}
-		cout << endl;
-	}
+	print(arr, ROWS, COLS);
 #endif // SORT
 
 #ifdef UniqueRandom
@@ -134,68 +120,114 @@ void main()
 	}
 	//Вывод массива после заполнения
 	cout << endl;
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			cout << brr[i][j] << tab;
-		}
-		cout << endl;
-	}
+	print(brr, ROWS, COLS);
 
 #endif//UniqueRandom
 
 #ifdef Comparison
-
-
 	// Сравнение элементов массива
 
 	cout << delimiter << "\nСравнение элементов массива" << endl;
 	mark = 0;
 	box = 0;
 	int counter = 0;
-	int crr[COLS][ROWS]{};
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			crr[i][j] = rand() % 10;
-		}
-	}
+	//Создание массива
+	int crr[ROWS][COLS]{};
+	create(crr, ROWS, COLS);
 
 	//Вывод массива после заполнения
 	cout << endl;
+	print(crr, ROWS, COLS);
+	//Сравнение
 	for (int i = 0; i < ROWS; i++)
 	{
 		for (int j = 0; j < COLS; j++)
 		{
-			cout << crr[i][j] << tab;
-		}
-		cout << endl;
-	}
-
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
+			counter = 0;
+			mark = 0;
 			for (int k = i; k < ROWS; k++)
 			{
-				for (int l = j-1; l < COLS; l++)
+				if (k == i)
 				{
-					if ((crr[i][j]) == crr[k][l])
+					for (int l = j + 1; l < COLS; l++)
 					{
-						counter++;//дальше обратное сравнение
+						if ((crr[i][j]) == crr[k][l])
+						{
+							counter++;
+						}
 					}
 				}
+				else
+				{
+					for (int l = 0; l < COLS; l++)
+					{
+						if ((crr[i][j]) == crr[k][l])
+						{
+							counter++;
+						}
+					}
+				}
+			}
+			if (counter > 0)//Если нашли совпадения
+			{
+				for (int k = i; k >= 0; k--)
+				{
+					if (k == i)
+					{
+						for (int l = j - 1; l >= 0; l--)
+						{
+							if (crr[i][j] == crr[k][l])
+							{
+								mark = 1;
+							}
+						}
+					}
+					else
+					{
+						for (int l = COLS; l >= 0; l--)
+						{
+							if (crr[i][j] == crr[k][l])
+							{
+								mark = 1;
+							}
+						}
+					}
+				}
+			}
+			if (mark == 0 && counter > 0)
+			{
+				cout << "\nЧисло " << crr[i][j] << " встречается " << counter << " раз." << endl;
 			}
 		}
 	}
 
 
 #endif // Comparison
-
-
 }
+void create(int arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			arr[i][j] = rand() % 10;
+		}
+	}
+}
+
+void print(int arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			cout << arr[i][j] << tab;
+		}
+		cout << endl;
+	}
+}
+
 
 
 
